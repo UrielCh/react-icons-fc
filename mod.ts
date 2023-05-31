b75a8a0aa32e7186f77af54b1fdbb77ad3812d89
urielch <uriel.chemouni@gmail.com>
2023-05-31 18:34:16 +0300

    bump version

--- mod.ts
+++ mod.ts
@@ -17,8 +17,8 @@
  *   "imports": {
  *     "preact":  "https://esm.sh/preact@10.15.1",
  *     "preact/": "https://esm.sh/preact@10.15.1/",
- *     "react-icons/fc":  "https://cdn.jsdelivr.net/gh/urielch/react-icons-fc@1.0.5/mod.ts",
- *     "react-icons/fc/": "https://cdn.jsdelivr.net/gh/urielch/react-icons-fc@1.0.5/ico/",
+ *     "react-icons/fc":  "https://cdn.jsdelivr.net/gh/urielch/react-icons-fc@1.0.6/mod.ts",
+ *     "react-icons/fc/": "https://cdn.jsdelivr.net/gh/urielch/react-icons-fc@1.0.6/ico/",
  *   }
  * }
  * ```
@@ -26,7 +26,7 @@
  * ## Direct import sample
  * 
  * ```ts
- * import { FcAbout } from "https://deno.land/x/react_icons_fc@1.0.5/mod.ts"
+ * import { FcAbout } from "https://deno.land/x/react_icons_fc@1.0.6/mod.ts"
  * ```
  * 
  * ## import_map import sample
