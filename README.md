01cfbefe3c8537484c60aaa8967c2faebee35338
urielch <uriel.chemouni@gmail.com>
2024-07-18 10:53:14 +0200

    update doc

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 [See available icons here](https://react-icons.deno.dev/fc)
 
-## install packahe
+## install the module
 
 ```bash
 deno add @preact-icons/fc
@@ -17,36 +17,15 @@ pnpm dlx jsr add @preact-icons/fc
 bunx jsr add @preact-icons/fc
 ```
 
-You may need to update your preact mapping to:
- - "preact": "npm:preact@10.22.1",
- - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- - "preact/hooks": "npm:preact@10.22.1/hooks",
-
-
-# Devicons icons for preact
-
-[![JSR](https://jsr.io/badges/@preact-icons/fc)](https://jsr.io/@preact-icons/fc)
-
-**License** [MIT](https://opensource.org/licenses/MIT)
-
-**Project** [https://github.com/icons8/flat-color-icons](https://github.com/icons8/flat-color-icons)
-
-[See available icons here](https://react-icons.deno.dev/fc)
-
-## install packahe
-
-```bash
-deno add @preact-icons/fc
-dnpx jsr add @preact-icons/fc
-pnpm dlx jsr add @preact-icons/fc
-bunx jsr add @preact-icons/fc
+You may need to update your preact mapping to avoid mixing JSR and http import:
+Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+```json
+{
+ "preact": "npm:preact@10.22.1",
+ "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ "preact/hooks": "npm:preact@10.22.1/hooks",
+}
 ```
-
-You may need to update your preact mapping to:
- - "preact": "npm:preact@10.22.1",
- - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- - "preact/hooks": "npm:preact@10.22.1/hooks",
-
 
 ## import an icon from all icons
 
@@ -65,4 +44,3 @@ or using default export
 ```ts
 import FcAbout from "react-icons/fc/FcAbout.ts"
 ```
-
--- mod.ts
+++ mod.ts
@@ -10,7 +10,7 @@
  * 
  * [See available icons here](https://react-icons.deno.dev/fc)
  * 
- * ## install packahe
+ * ## install the module
  * 
  * ```bash
  * deno add @preact-icons/fc
@@ -19,36 +19,15 @@
  * bunx jsr add @preact-icons/fc
  * ```
  * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
- * 
- * # Devicons icons for preact
- * 
- * [![JSR](https://jsr.io/badges/@preact-icons/fc)](https://jsr.io/@preact-icons/fc)
- * 
- * **License** [MIT](https://opensource.org/licenses/MIT)
- * 
- * **Project** [https://github.com/icons8/flat-color-icons](https://github.com/icons8/flat-color-icons)
- * 
- * [See available icons here](https://react-icons.deno.dev/fc)
- * 
- * ## install packahe
- * 
- * ```bash
- * deno add @preact-icons/fc
- * dnpx jsr add @preact-icons/fc
- * pnpm dlx jsr add @preact-icons/fc
- * bunx jsr add @preact-icons/fc
+ * You may need to update your preact mapping to avoid mixing JSR and http import:
+ * Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+ * ```json
+ * {
+ *  "preact": "npm:preact@10.22.1",
+ *  "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  "preact/hooks": "npm:preact@10.22.1/hooks",
+ * }
  * ```
- * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
  * 
  * ## import an icon from all icons
  * 
@@ -67,8 +46,8 @@
  * ```ts
  * import FcAbout from "react-icons/fc/FcAbout.ts"
  * ```
- * 
  * @module
+ * 
  */
 
 import { GenIcon } from "./deps.ts";
